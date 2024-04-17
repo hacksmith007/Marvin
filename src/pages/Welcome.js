296c5a5ef35ecbb9f89e67f225e794a4509fe885
hacksmith007 <rahulranjan87@outlook.com>
2024-04-18 02:21:52 +0530

    Success : Added Bootstrap and Navbar Sample
    Warning: 0
    
    Error: 0

---
+++ src/pages/Welcome.js
@@ -0,0 +1,11 @@
+import React from "react";
+import NavBar from "../components/topNav";
+
+
+const Welcome = () => {
+    return (
+        <NavBar title="RGpt" aboutText="About" />
+    )
+}
+
+export default Welcome;
